641b577abf79801c27806ac73f6bcaf2018fc5ec
0xMAJIMAK <derrick@suberra.com>
2023-04-23 01:43:15 +0800

    feat api ready

--- src/pages/api/position.ts
+++ src/pages/api/position.ts
@@ -1,5 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { ethers } from "ethers";
 import prisma from "../../utils/prisma";
+import { executeZap } from "../../utils/executeZap";
+import { getNextPeriod } from "../../utils/getNextPeriod";
+import dayjs from "dayjs";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
@@ -7,10 +11,48 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (!safeAddress || typeof safeAddress !== "string") {
       return res.status(500).json({ error: "Invalid safe address" });
     }
-    return await prisma.positions.findMany({
+    return await prisma.position.findMany({
       where: {
         address: safeAddress,
       },
     });
   }
+  if (req.method === "POST") {
+    const { safeAddress, sourceToken, pairedToken, totalAmount, interval, interval_count } = req.body;
+    if (!safeAddress || typeof safeAddress !== "string") {
+      return res.status(500).json({ error: "Invalid safe address" });
+    }
+    if (!sourceToken || typeof sourceToken !== "string" || !ethers.utils.isAddress(sourceToken)) {
+      return res.status(500).json({ error: "Invalid source token" });
+    }
+    if (!pairedToken || typeof pairedToken !== "string" || !ethers.utils.isAddress(pairedToken)) {
+      return res.status(500).json({ error: "Invalid paired token" });
+    }
+    if (!totalAmount || typeof totalAmount !== "number") {
+      return res.status(500).json({ error: "Invalid source amount" });
+    }
+
+    const tx = await executeZap(safeAddress, sourceToken, pairedToken, totalAmount);
+    if (!tx) return res.status(500).json({ error: "Failed to execute zap" });
+
+    const now = dayjs();
+    const { last_swap_at, next_swap_at } = getNextPeriod(now.toDate(), interval, interval_count);
+    const result = await prisma.position.create({
+      data: {
+        address: safeAddress,
+        source_token: sourceToken,
+        paired_token: pairedToken,
+        total_amount: totalAmount,
+        last_swap_at,
+        next_swap_at,
+        interval,
+        interval_count,
+        swap_success_tx_hash: [tx.hash],
+      },
+    });
+
+    return res.status(200).json({ result });
+  }
+
+  return res.status(405).end("Method not allowed");
 }
---
+++ src/pages/api/renew.ts
@@ -0,0 +1,58 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../utils/prisma";
+import dayjs from "dayjs";
+import { getNextPeriod } from "../../utils/getNextPeriod";
+import { executeZap } from "../../utils/executeZap";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const SECRET = process.env.SECRET_TOKEN;
+  if (req.headers.authorization !== SECRET) return res.status(401).end("Unauthorized");
+  if (req.method === "POST") {
+    const now = dayjs();
+    const toRenew = await prisma.position.findMany({
+      where: {
+        next_swap_at: {
+          lte: now.toDate(),
+        },
+        failure_tx_count: {
+          lt: 5,
+        },
+      },
+    });
+
+    const results = await Promise.all(
+      toRenew.map(async (position) => {
+        const tx = await executeZap(position.address, position.source_token, position.paired_token, position.total_amount.toNumber());
+        if (!tx) {
+          return prisma.position.update({
+            where: {
+              id: position.id,
+            },
+            data: {
+              failure_tx_count: position.failure_tx_count,
+            },
+          });
+        } else {
+          const { last_swap_at, next_swap_at } = getNextPeriod(now.toDate(), position.interval, position.interval_count);
+
+          return prisma.position.update({
+            where: {
+              id: position.id,
+            },
+            data: {
+              last_swap_at,
+              next_swap_at,
+              swap_success_tx_hash: {
+                push: tx.hash,
+              },
+            },
+          });
+        }
+      })
+    );
+
+    return res.status(200).json({ results });
+  }
+
+  return res.status(405).end("Method not allowed");
+}
